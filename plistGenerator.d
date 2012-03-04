66f1b85790bc445e51a97e1dfde6687b2edcc668
Joshua Niehus <jm.niehus@gmail.com>
2012-03-03 22:15:49 -0800

    updated to handle strings
    delim and tokens

--- plistGenerator.d
+++ plistGenerator.d
@@ -5,7 +5,39 @@ private string plistSpecialTokens = "      </dict>    </dict>    <dict>     
 ";    // closes off plistSpecialTokens <dict> <dict>  
 private string plistPhobos = "       </dict>     </dict>     <dict>       <key>description</key>       <string>phobos modules</string>       <key>color</key>       <string>ClassColor</string>       <key>strings</key>       <dict>
 ";    // closes off the header's <array> and plistPhobos <dict> <dict>
-private string plistBlocks = "      </dict>    </dict>     </array> <key>ranges</key><dict><key>BlockComment</key><dict><key>color</key><string>CommentColor</string><key>end</key><string>*/</string><key>start</key><string>/*</string></dict><key>NestedBlockComment</key><dict><key>color</key><string>CommentColor</string><key>end</key><string>+/</string><key>start</key><string>/+</string></dict>		<key>DoubleQuote</key><dict><key>color</key><string>StringColor</string><key>end</key><string>&quot;</string>								<key>escape</key><string>\\</string><key>start</key><string>&quot;</string></dict><key>ReDoubleQuote</key><dict><key>color</key><string>StringColor</string><key>end</key><string>&quot;</string><key>escape</key><string>\\</string><key>start</key><string>r&quot;</string></dict>									<key>OneLineComment</key><dict><key>color</key><string>CommentColor</string><key>end</key><string>
+private string plistBlocks = "      </dict>    </dict>     </array> <key>ranges</key><dict><key>BlockComment</key><dict><key>color</key><string>CommentColor</string><key>end</key><string>*/</string><key>start</key><string>/*</string></dict><key>NestedBlockComment</key><dict><key>color</key><string>CommentColor</string><key>end</key><string>+/</string><key>start</key><string>/+</string></dict>		<key>SingleQuote</key>
+<dict>
+<key>color</key>
+<string>StringColor</string>
+<key>end</key>
+<string>'</string>
+<key>escape</key>
+<string>\\</string>
+<key>start</key>
+<string>'</string>
+</dict><key>DoubleQuote</key><dict><key>color</key><string>StringColor</string><key>end</key><string>&quot;</string><key>escape</key><string>\\</string><key>start</key><string>&quot;</string></dict><key>ReDoubleQuote</key><dict><key>color</key><string>StringColor</string><key>end</key><string>&quot;</string><key>escape</key><string>\\</string><key>start</key><string>r&quot;</string></dict>	
+<key>DelimQuote</key>
+<dict>
+<key>color</key>
+<string>StringColor</string>
+<key>end</key>
+<string>&quot;</string>
+<key>escape</key>
+<string>\\</string>
+<key>start</key>
+<string>q&quot;</string>
+</dict>	
+<key>TokenQuote</key>
+<dict>
+<key>color</key>
+<string>StringColor</string>
+<key>end</key>
+<string>}</string>
+<key>escape</key>
+<string>\\</string>
+<key>start</key>
+<string>q{</string>
+</dict>									<key>OneLineComment</key><dict><key>color</key><string>CommentColor</string><key>end</key><string>
 </string><key>start</key><string>#!</string></dict><key>OneLineComment2</key><dict><key>color</key><string>CommentColor</string><key>end</key><string>
 </string><key>start</key><string>//</string></dict>	</dict><key>numbers</key><dict><key>color</key><string>NumberColor</string><key>pattern</key><string>(?&lt;=[^\\w\\d]|^)(((([0-9]+\\.[0-9]*)|(\\.[0-9]+))([eE][+\\-]?[0-9]+)?[fFlL]?)|((([1-9][0-9]*)|0[0-7]*|(0[xX][0-9a-fA-F]+))(([uU][lL]?)|([lL][uU]?))?))(?=[^\\w\\d]|$)</string></dict><key>completions</key><array><dict><key>word</key><string>for ()</string><key>insertion</key><string>for (&lt;!#expressions#!&gt;) {}</string><key>prefix</key><string>all</string><key>completion</key><true/></dict><dict><key>word</key><string>foreach ()</string><key>insertion</key><string>foreach (&lt;!#expressions#!&gt;) {}</string><key>prefix</key><string>all</string><key>completion</key><true/></dict>	<dict><key>word</key><string>foreach_reverse ()</string><key>insertion</key><string>foreach_reverse (&lt;!#expressions#!&gt;) {}</string><key>prefix</key><string>all</string><key>completion</key><true/></dict>			<dict><key>word</key><string>if ()</string><key>insertion</key><string>if (&lt;!#condition#!&gt;) {}</string><key>prefix</key><string>all</string><key>completion</key><true/></dict><dict><key>word</key><string>if () else {}</string><key>insertion</key><string>if (&lt;!#condition#!&gt;) {} else {}</string><key>prefix</key><string>all</string><key>completion</key><true/></dict><dict><key>word</key><string>else if ()</string><key>insertion</key><string>else if (&lt;!#condition#!&gt;) {}</string><key>prefix</key><string>all</string><key>completion</key><true/></dict><dict><key>word</key><string>else {}</string><key>insertion</key><string>else {&lt;!##!&gt;}</string><key>prefix</key><string>all</string><key>completion</key><true/></dict><dict><key>word</key><string>while ()</string><key>insertion</key><string>while (&lt;!#condition#!&gt;) {}</string><key>prefix</key><string>all</string><key>completion</key><true/></dict><dict><key>word</key><string>do {} while ()</string><key>insertion</key><string>do {} while (&lt;!#condition#!&gt;);</string><key>prefix</key><string>all</string><key>completion</key><true/></dict><dict><key>word</key><string>switch ()</string><key>insertion</key><string>switch (&lt;!#expression#!&gt;) {default:break;}</string><key>prefix</key><string>all</string><key>completion</key><true/></dict>";// closes <array> from blocks and headers <dict><plist>
 private string plistFooter = "</array><key>wordCharacters</key><string>_</string></dict></plist>";string plistWrap(string key) {    return "        <key>" ~ key ~ "</key>\n        <string></string>\n";  }void main(){    string[] deprecatedModules = ["std.cpuid", "std.ctype", "std.date", "std.gregorian", "std.regexp"];    string dlangOrgStd = "/Users/joshuaniehus/GIT/d-programming-language.org/std.ddoc";
